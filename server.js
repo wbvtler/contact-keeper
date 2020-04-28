c71c500d6c2b89526a29eba68119ff441e8d25a5
wbvtler <wesleybutler.code@gmail.com>
2020-04-28 15:29:34 -0500

    Set up Express server with initial routes

--- server.js
+++ server.js
@@ -7,9 +7,9 @@ app.get('/', (req, res) =>
 );
 
 // define routes
-// app.use('/api/users', require('./routes/users'))
-// app.use('/api/auth', require('./routes/auth'))
-// app.use('/api/contacts', require('./routes/contacts'))
+app.use('/api/users', require('./routes/users'));
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/contacts', require('./routes/contacts'));
 
 const PORT = process.env.PORT || 5000;
 
