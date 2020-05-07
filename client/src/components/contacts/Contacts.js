38e2521164dfc5327cd8aff866c15a69214ffdf8
wbvtler <wesleybutler.code@gmail.com>
2020-05-07 17:53:16 -0500

    Add transition animations to contact cards

--- client/src/components/contacts/Contacts.js
+++ client/src/components/contacts/Contacts.js
@@ -1,4 +1,5 @@
 import React, { Fragment, useContext } from 'react';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import ContactItem from './ContactItem';
 import ContactContext from '../../context/contact/contactContext';
 
@@ -11,16 +12,22 @@ const Contacts = () => {
 
   return (
     <Fragment>
-      {
-        // if contacts are filtered, show filtered, otherwise show all
-        filtered !== null
-          ? filtered.map((contact) => (
-              <ContactItem key={contact.id} contact={contact} />
-            ))
-          : contacts.map((contact) => (
-              <ContactItem key={contact.id} contact={contact} />
-            ))
-      }
+      <TransitionGroup>
+        {
+          // if contacts are filtered, show filtered, otherwise show all
+          filtered !== null
+            ? filtered.map((contact) => (
+                <CSSTransition key={contact.id} timeout={500} classNames="item">
+                  <ContactItem contact={contact} />
+                </CSSTransition>
+              ))
+            : contacts.map((contact) => (
+                <CSSTransition key={contact.id} timeout={500} classNames="item">
+                  <ContactItem contact={contact} />
+                </CSSTransition>
+              ))
+        }
+      </TransitionGroup>
     </Fragment>
   );
 };
