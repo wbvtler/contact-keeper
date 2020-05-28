70dc71ac97d99045fcd20ae5717932caac987d0b
wbvtler <wesleybutler.code@gmail.com>
2020-05-28 15:31:32 -0500

    Connect add contact from UI to db

--- client/src/components/contacts/Contacts.js
+++ client/src/components/contacts/Contacts.js
@@ -17,12 +17,20 @@ const Contacts = () => {
           // if contacts are filtered, show filtered, otherwise show all
           filtered !== null
             ? filtered.map((contact) => (
-                <CSSTransition key={contact.id} timeout={500} classNames="item">
+                <CSSTransition
+                  key={contact._id}
+                  timeout={500}
+                  classNames="item"
+                >
                   <ContactItem contact={contact} />
                 </CSSTransition>
               ))
             : contacts.map((contact) => (
-                <CSSTransition key={contact.id} timeout={500} classNames="item">
+                <CSSTransition
+                  key={contact._id}
+                  timeout={500}
+                  classNames="item"
+                >
                   <ContactItem contact={contact} />
                 </CSSTransition>
               ))
--- client/src/context/contact/contactReducer.js
+++ client/src/context/contact/contactReducer.js
@@ -5,6 +5,7 @@ import {
   CLEAR_CURRENT,
   UPDATE_CONTACT,
   FILTER_CONTACTS,
+  CONTACT_ERROR,
   CLEAR_FILTER
 } from '../types';
 
@@ -55,6 +56,11 @@ export default (state, action) => {
       return {
         ...state,
         filtered: null
+      };
+    case CONTACT_ERROR:
+      return {
+        ...state,
+        error: action.payload
       };
     default:
       return state;
